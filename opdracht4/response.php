b2dbc762c1680a3825dc386a576db3ed43e5e385
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2018-01-19 09:57:34 +0100

    Added If Else statements.

--- opdracht4/response.php
+++ opdracht4/response.php
@@ -4,12 +4,41 @@
 
     $plus = $_POST ['plus'];
 
-    echo 'Om te beginnen heeft u ' . $plus . ' ingevoert. <br>';
+    if ( $plus == '19' )
+    {
+        echo 'Om te beginnen heeft u ' . $plus . ' ingevoert. <br>';
+    }
+    else if ( $plus == '21' )
+    {
+        echo 'Om te beginnen heeft u ' . $plus . ' ingevoert. <br>';
+    }
 
     $tachanka = $_POST ['tachanka'];
 
-    echo 'Daarna heeft U; ' . $tachanka . ' gestemt op het buffen van Tachanka. <br>';
+    if ($tachanka == 'Ja')
+    {
+        echo 'Daarna heeft U; ' . $tachanka . ' gestemt op het buffen van Tachanka, hij krijgt nu een tweede LMG. <br>';
+    }
+    else if( $tachanka == 'Nee')
+    {
+        echo 'Daarna heeft U;' . $tachanka . ' gestemt op het buffen van Tachanka, hij is nu 2 armor en 1 speed. <br>';
+    }
+    else if($tachanka == 'Allebei')
+    {
+        echo 'Daarna heeft U;' . $tachanka . ' gestemt op het buffen van Tachanka, hij blijft nu hetzelfde. <br>';
+    }
 
     $cijfer = $_POST ['cijfer'];
 
-    echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor het cijfer.<br>';
+    if ( $cijfer == '6' )
+    {
+        echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor het zesje.<br>';
+    }
+    else if ( $cijfer == '8' )
+    {
+        echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor een acht.<br>';
+    }
+    else if ( $cijfer == '10' )
+    {
+        echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor een tien!<br>';
+    }
