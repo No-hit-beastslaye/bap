f896c08fa93c25c0a952b1770c3650cee085593d
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2018-01-19 09:56:53 +0100

    Update sendmail.php

--- opdracht5/sendmail.php
+++ opdracht5/sendmail.php
@@ -1,44 +1,12 @@
 <?php
 
-    echo '<h1> Dit is wat U heeft ingevoert. </h1>';
+$to = '24853@ma-web.nl';
+$subject = $_POST ['subject'];
+$message = $_POST ['message'];
+$headers = 'From: '. $_POST ['from'];
 
-    $plus = $_POST ['plus'];
+mail($to, $subject, $message, $headers);
 
-    if ( $plus == '19' )
-    {
-        echo 'Om te beginnen heeft u ' . $plus . ' ingevoert. <br>';
-    }
-    else if ( $plus == '21' )
-    {
-        echo 'Om te beginnen heeft u ' . $plus . ' ingevoert. <br>';
-    }
+echo 'Uw E-mail is succesvol verstuurd.';
 
-    $tachanka = $_POST ['tachanka'];
-
-    if ($tachanka == 'Ja')
-    {
-        echo 'Daarna heeft U; ' . $tachanka . ' gestemt op het buffen van Tachanka, hij krijgt nu een tweede LMG. <br>';
-    }
-    else if( $tachanka == 'Nee')
-    {
-        echo 'Daarna heeft U;' . $tachanka . ' gestemt op het buffen van Tachanka, hij is nu 2 armor en 1 speed. <br>';
-    }
-    else if($tachanka == 'Allebei')
-    {
-        echo 'Daarna heeft U;' . $tachanka . ' gestemt op het buggen van Tachanka, hij blijft nu hetzelfde. <br>';
-    }
-
-    $cijfer = $_POST ['cijfer'];
-
-    if ( $cijfer == '6' )
-    {
-        echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor het zesje.<br>';
-    }
-    else if ( $cijfer == '8' )
-    {
-        echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor een acht.<br>';
-    }
-    else if ( $cijfer == '10' )
-    {
-        echo 'En als laatste, heeft u ' . $cijfer . ' ingevoert, bedankt voor een tien!<br>';
-    }
+echo 'U heeft ' . $subject . ' verstuurd, met als bericht; "' . $message . '".';
