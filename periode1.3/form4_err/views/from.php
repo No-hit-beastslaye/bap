8b5a6d9f1fb04222d49d28760839d957864b5077
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2018-04-04 11:51:40 +0200

    Add files via upload

---
+++ periode1.3/form4_err/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>Brandon Ruigrok uit klas GD1B</h3>
+<hr>
+<pre>
+  <form method="post">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="email">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact' value="Send">
+  </form>
+</pre>
+<hr>
